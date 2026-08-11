351d2998574bc8c32827e160d2f3b971eab52006
Katy <k.l.scott16@gmail.com>
2026-08-11 20:39:13 +0000

    feat: click cli inputs for medsam2 prepare

--- src/ab_testing/models/medsam2/prepare.py
+++ src/ab_testing/models/medsam2/prepare.py
@@ -1,10 +1,9 @@
-import argparse
+import click
 import logging
 
 
 from damply import dirs
 from pathlib import Path
-from typing import Literal
 
 from ab_testing.utils.conver_nifti_to_recist_npz import run_dataset, MIN_LABEL_SLICES, MIN_RECIST_MM
 
@@ -20,38 +19,101 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-ANATOMICAL_BUCKETS = Literal[
-    "lung"
+ANATOMICAL_BUCKETS = [
+    "lung",
     "soft_tissue",   # includes heart
     "bone",
-    "brain"
+    "brain",
 ]
 
+@click.command(no_args_is_help=True)
+@click.argument(
+    "dataset",
+    type=str,
+)
+@click.argument(
+    "mit_directory",
+    type=click.Path(
+        file_okay=False, dir_okay=True, writable=False, path_type=Path, resolve_path=True, exists=True
+    ),
+)
+@click.argument(
+    "output_directory",
+    type=click.Path(
+        file_okay=False, dir_okay=True, writable=True, path_type=Path, resolve_path=True
+    ),
+)
+@click.option(
+    "--anat-window",
+    type=click.Choice(ANATOMICAL_BUCKETS),
+    required=True,
+    help=f"Anatomical windowing to apply for this dataset. Must be one of {ANATOMICAL_BUCKETS}."
+)
+@click.option(
+    "--min-slices",
+    type=int,
+    default=MIN_LABEL_SLICES,
+    show_default=True,
+    help="Minimum labelled slice count required for npz conversion."
+)
+@click.option(
+    "--min-recist-mm",
+    type=float,
+    default=MIN_RECIST_MM,
+    show_default=True,
+    help="Minimum RECIST diameter measurement required for npz conversion in millimetres (mm)."
+)
+@click.option(
+    "--workers",
+    type=int,
+    default=24,
+    help="Number of parallel processes to use."
+)
+@click.option(
+    "--overwrite",
+    is_flag=True,
+    help="Whether to overwrite existing files."
+)
 def prepare_data(
     dataset: str, 
-    imgtools_path: str | Path, 
-    output_path: str | Path,
-    anat_window: ANATOMICAL_BUCKETS = 'soft-tissue',
+    mit_directory: str | Path, 
+    output_directory: str | Path,
+    anat_window: str,
     min_slices: int = MIN_LABEL_SLICES,
     min_recist_mm: float = MIN_RECIST_MM,
     workers: int = 24,
     overwrite: bool = False,
-    ) -> None:
-    """Process a med-imagetools nifti dataset by pair of image-masks and save into a npz:
-    * imgs: The image, windowed based on the anatomical window specified,
-    * gts: The corresponding 3D segmentation mask, 
-    * spacing: Original image/mask spacing, from SimpleITK
-    * direction: Original image/mask direction, from SimpleITK
-    * origin: Original image/mask origin, from SimpleTIK 
-    * reader: What library was used to load in the image/mask, will be sitk or nibabel_orthofix
-    * recist: Reverse-engineered RECIST longest diameter annotation derived from gts
+    ) -> int:
+    """Process a med-imagetools nifti dataset by pair of image-masks and save into npzs.
+    
+    Converted npz contains:
+        * imgs: The image, windowed based on the anatomical window specified,
+        * gts: The corresponding 3D segmentation mask, 
+        * spacing: Original image/mask spacing, from SimpleITK
+        * direction: Original image/mask direction, from SimpleITK
+        * origin: Original image/mask origin, from SimpleTIK 
+        * reader: What library was used to load in the image/mask, will be sitk or nibabel_orthofix
+        * recist: Reverse-engineered RECIST longest diameter annotation derived from gts
     
     Based on min_slices and min_recist_mm, samples will be pruned and not saved as npz if they fall below the threshold.
+
+    Args:
+        dataset: str, dataset name
+        mit_directory: str | Path, pathway to the image and mask directory
+        output_directory: str | Path, output directory npz files will be saved to
+        anat_window: anatomical windowing to apply for this dataset
+        min_slices: int, minimum number of axial slices with label for lesion inclusion (default 5)
+        min_recist_mm: float, RECIST diameter threshold in millimetres for lesion inclusion (default 10)
+        workers: int, number of parallel processes to use (default 24)
+        overwrite: bool, whether to reconvert every case (default False, skip existing npz)
+    
+    Returns:
+        0 if successful, 1 if errors occurred.
     """
     written, skipped, filtered, errors, dropped_lesions, _z_kept, _z_total = run_dataset(
         ds = dataset,
-        root = Path(imgtools_path),
-        out_root = Path(output_path),
+        root = Path(mit_directory),
+        out_root = Path(output_directory),
         workers = workers,
         with_recist = True,
         overwrite = overwrite,
@@ -73,21 +135,4 @@ def prepare_data(
             
 
 if __name__ == "__main__":
-    prepare_data(
-        dataset='NSCLC-Radiomics',
-        imgtools_path= dirs.RAWDATA / 'TCIA_NSCLC-Radiomics' / 'images' / 'mit_NSCLC-Radiomics',
-        output_path = dirs.PROCDATA / 'TCIA_NSCLC-Radiomics' / 'images' / 'npz_medsam2_data',
-        anat_window = 'lung',
-        min_slices= 3,
-    )
-
-
-
-    # argparser = argparse.ArgumentParser()
-    # argparser.add_argument("--imgtools_path", type=str, required=True)
-    # argparser.add_argument("--output_path", type=str, required=True)
-    # args = argparser.parse_args()
-
-    # # dirs.PROCDATA / 'TCIA_NSCLC-Radiomics' / 'images' / 'npz_medsam2_data'
-
-    # prepare_data(args.imgtools_path, args.output_path)
+    prepare_data()
